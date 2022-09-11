aab83ea037a600ef2a13a357a279dc13fb388f71
abraxax777 <jannyalexander@gmail.com>
2022-09-11 00:07:09 -0500

    hola mundo

--- src/main/java/com/Udea/ciclo3/Ciclo3Application.java
+++ src/main/java/com/Udea/ciclo3/Ciclo3Application.java
@@ -2,12 +2,22 @@ package com.Udea.ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/hello world")
+	public String hello(){
+		return "hola coclo 3 ...lo voy a sacar adelante!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+		System.out.println("hola mundo");
 	}
 
 }
