9c436f6d66f0c981916e49e6a956041e2e45bdf4
abraxax777 <jannyalexander@gmail.com>
2022-09-13 06:39:40 -0500

    crear entidad empleado

--- src/main/java/entidades/Empleado.java
+++ src/main/java/entidades/Empleado.java
@@ -2,5 +2,67 @@ package entidades;
 
 import javax.persistence.*;
 
+@Entity
+@Table(name="Empleado")
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String correo;
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
+    private String rol;
+
+    public Empleado(int id, String nombre, String correo, Empresa empresa, String rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public Empleado() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
 }
